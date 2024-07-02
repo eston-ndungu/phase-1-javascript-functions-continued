089f2fbffa9ca85ef5f8515528dc2a415fd70788
ESTON NDUNGU <estonndungu1@gmail.com>
2024-07-02 15:39:12 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") { //Function name = saturdayFun, parameter = (activity = "roller-skate")
+    return `This Saturday, I want to ${activity}!` // Join strings using interpolation
+}
+
+// Define a function using a function expression
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+
+//
+
+function wrapAdjective(msg = "*") {
+    return function (adj = "special") {
+        return `You are ${msg}${adj}${msg}!`
+    }
+       
+}
+
